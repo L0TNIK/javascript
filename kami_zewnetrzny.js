57121ffb3e4ee3033858020aed6ba4ac9863f0f3
L0TNIK <144437056+L0TNIK@users.noreply.github.com>
2024-10-03 10:57:39 +0200

    Add files via upload

--- kami_zewnetrzny.js
+++ kami_zewnetrzny.js
@@ -1,13 +1,8 @@
-document.write("ostatnia modyfikacja strony".fontcolor("#800000").strike().bold().fontsize(7)+"<br>"); 
-document.write(document.lastModified); 
-
-
-
-
-
-
-
-
-
-
-
+document.write(
+  "ostatnia modyfikacja strony"
+    .fontcolor("#800000")
+    .strike()
+    .bold()
+    .fontsize(7) + "<br>"
+);
+document.write(document.lastModified);
